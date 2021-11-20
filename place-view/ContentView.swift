9882c10e4c58d68fd851cd4985370990c1495b44
Jérémy Jousse <jeremy.jousse@gmail.com>
2021-11-20 01:52:24 +0100

    Initial Commit

---
+++ place-view/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  place-view
+//
+//  Created by Jérémy Jousse on 20/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ place-view/place_viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  place_viewApp.swift
+//  place-view
+//
+//  Created by Jérémy Jousse on 20/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct place_viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
