13bbf9ff649ab3150e7b144944fe70c8699ff6a5
Jérémy Jousse <jeremy.jousse@decathlon.com>
2026-01-05 21:19:27 +0100

    feat(webapp): :sparkles: add deno adapter

--- webapp/svelte.config.js
+++ webapp/svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterDeno from '@deno/svelte-adapter';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,10 +9,8 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		// Select adapter based on environment variable
+		adapter: process.env.ADAPTER === 'deno' ? adapterDeno() : adapterAuto()
 	}
 };
 
