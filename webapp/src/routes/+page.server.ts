c1123a2b197267859b9c033256460e6abfe4ce48
Jérémy Jousse <jeremy.jousse@decathlon.com>
2026-01-12 09:12:52 +0100

    refactor(webapp): :art: remove comment

--- webapp/src/routes/+page.server.ts
+++ webapp/src/routes/+page.server.ts
@@ -2,7 +2,6 @@ import type { PageServerLoad } from './$types';
 import { placeRepository } from '$lib/server';
 
 export const load: PageServerLoad = async () => {
-	// Return only a few featured places for demonstration
 	const result = await placeRepository.search('', 12, 0);
 
 	return {
