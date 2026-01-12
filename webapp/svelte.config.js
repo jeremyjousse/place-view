421f029e74e0ee690e4c5c951b4636eed730cc2d
Jérémy Jousse <jeremy.jousse@decathlon.com>
2026-01-12 07:51:40 +0100

    feat(webapp): :sparkles: fix

--- webapp/svelte.config.js
+++ webapp/svelte.config.js
@@ -4,12 +4,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// Select adapter based on environment variable
 		adapter: process.env.ADAPTER === 'deno' ? adapterDeno() : adapterAuto()
 	}
 };
